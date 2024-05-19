de1f333902358426888928732c2e8d97b6308eb5
hieu1966hn <hieu1966hn@gmail.com>
2024-05-19 09:45:09 +0700

    new code SS2

---
+++ SS2/SS2_lythuyet.md
@@ -0,0 +1,26 @@
+Buổi 1:
+- Giới thiệu ngôn ngữ Python - Sức mạnh của Python với CS:
+- Nhập thông tin: input()
+- Vẽ bằng turtle: Python
+
+
+Buổi 2:
+- Kiểu giá trị String
++ Khai báo biến String
++ Thao tác với String: index, độ dài String, cộng/ nhân String.
+- Kiểu giá trị số:
++ Phân biệt được: int và float
++ Các toán tử với số: +, -, *, /, **, %, //
++ Phân biệt "5" và 5
++ Biết cách chuyển từ String => Số và ngược lại.
+
+
+Đề bài luyện tập;
+1. In ra terminal câu "xin chào tên của 3 thành viên trong lớp", Mỗi tên một dòng
+2. Hỏi người dùng nhập tên, sau đó in ra câu chào người đó.
+3. Viết chương trình tính tổng, hiệu, tích, thương 2 số do người dùng nhập vào.
+4. Sử dụng turtle vẽ hình lục giác, ngũ giác.
+
+Lưu ý: Không sử dụng AI và Internet trong quá trình làm bài tập.
+
+Điểm cộng: Trí, Lê Anh (Khuê còn hơi lỗi).
---
+++ SS2/ss2.py
@@ -0,0 +1,51 @@
+#### Kiểu dữ liệu String
+## khai báo biến String: tenBien = KDL
+# str1 = "Hello World!"
+# str2 = "Hi!"
+# str3 = """Đây là đoạn chuỗi được viết trong 3 dấu ngoặc kép
+# Đây là dòng thứ 2
+# Đây là dòng thứ 3
+# """
+## In ra biến String
+# print(str1)
+# print(str3) ## Đối với 3 dấu ngoặc kép: Có thể sử dụng để tạo chuỗi đa dòng.
+
+## In ra chữ "l" thứ 2 trong str1: index bắt đầu từ 0 -> ... Vậy index đại diện cho vị trí ký tự trong String
+## Index thường bắt đầu từ vị trí số 0.
+# print(str1[3])
+
+
+## In ra số lượng ký tự trong chuỗi: len()
+# print(len(str3))
+
+## Cộng String: string + string => string
+# print(str1 + " " + str2)
+
+
+### Truyền biến vào trong chuỗi
+# job = input("Mời người dùng nhập vào nghề nghiệp")
+# print("I am a " + job)
+# print(f"I am a {job}")
+
+### Tách chuỗi: Hello World!
+# str1_tachChuoi = str1[2:11]
+# print(str1_tachChuoi)
+
+
+### Kiểu dữ liệu Số: int và float
+
+### Toán tử: +, -, *, /, %, **
+# print(20/5*2)
+
+
+# a = 1
+# b = 2
+# print(
+#     a > b or 3 < 2
+# )  # Ví dụ về phép toán or: tìm giá trị True. Nếu không thấy True => false
+
+# print(a > 0 and a > b)  # Toán tử and: Tìm giá trị false. Nếu không thấy False => True
+
+## Toán tử not: Ngược lại so với giá trị ban đầu.
+# c = True
+# print(not c)
